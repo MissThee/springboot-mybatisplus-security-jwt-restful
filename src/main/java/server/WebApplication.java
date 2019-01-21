3f0da5d79e74cd606eb1c51e6cdf05fb34d9bd64
mateng <1163182715@qq.com>
2019-01-21 10:08:55 +0800

    update model config

--- src/main/java/server/WebApplication.java
+++ src/main/java/server/WebApplication.java
@@ -1,14 +1,9 @@
 package server;
 
-import com.corundumstudio.socketio.SocketIOServer;
-import com.corundumstudio.socketio.annotation.SpringAnnotationScanner;
-import io.swagger.annotations.Api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication//相当于@Configuration,@EnableAutoConfiguration和 @ComponentScan 并具有他们的默认属性值
 @EnableSwagger2//启用swagger2
