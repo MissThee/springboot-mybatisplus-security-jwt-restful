88f28f18b5483a649c484c03598ea11d18dcb2cd
mateng <1163182715@qq.com>
2019-01-21 20:50:23 +0800

    update

--- src/main/java/server/WebApplication.java
+++ src/main/java/server/WebApplication.java
@@ -2,13 +2,19 @@ package server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication//相当于@Configuration,@EnableAutoConfiguration和 @ComponentScan 并具有他们的默认属性值
 //@EnableSwagger2//启用swagger2
 @EnableAsync//启用异步调用
-public class WebApplication {
+public class WebApplication  extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
