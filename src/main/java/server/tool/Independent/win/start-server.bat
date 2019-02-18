94e679de09db0165890d0d09a68945493ca6e5be
mateng <1163182715@qq.com>
2019-02-18 10:29:02 +0800

    add multi application config

--- src/main/java/server/tool/Independent/win/start-server.bat
+++ src/main/java/server/tool/Independent/win/start-server.bat
@@ -12,11 +12,12 @@ title Project port:%port%
 ::kill process by port
 @echo off
 for /f "tokens=5" %%i in ('netstat -aon ^| findstr ":%port%"') do (
-    set n=%%i
-	
+	if %%i neq 0 (
+		set n=%%i
+	)
 )
-if defined n ( 
+if defined n (
 	taskkill /f /pid %n% 
 )
-java  -jar ../../../../../../../target/demo-0.0.1-SNAPSHOT.jar --server.port=%port% --spring.datasource.primary.driver-class-name=oracle.jdbc.OracleDriver --spring.datasource.primary.jdbc-url=jdbc:oracle:thin:@192.168.1.6:1521:cbmdb --spring.datasource.primary.username=cbmdb --spring.datasource.primary.password=cbmdb1qazxsw2   --logging.level.root=info --logging.level.server=info --logging.level.server.config.log=debug --logging.level.server.db.primary=info --logging.level.server.db.secondary=info
+java -jar ../../../../../../../target/demo-0.0.1-SNAPSHOT.jar --server.port=%port% --spring.datasource.primary.driver-class-name=oracle.jdbc.OracleDriver --spring.datasource.primary.jdbc-url=jdbc:oracle:thin:@192.168.1.6:1521:cbmdb --spring.datasource.primary.username=cbmdb --spring.datasource.primary.password=cbmdb1qazxsw2   --logging.level.root=info --logging.level.server=info --logging.level.server.config.log=debug --logging.level.server.db.primary=info --logging.level.server.db.secondary=info
 pause & exit
