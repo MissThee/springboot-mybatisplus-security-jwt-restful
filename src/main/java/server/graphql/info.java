02f9bd0e88da5be18b15c16ce988afa2c9bfd09a
mateng <1163182715@qq.com>
2019-02-22 12:01:11 +0800

    update

--- src/main/java/server/graphql/info.java
+++ src/main/java/server/graphql/info.java
@@ -1,4 +1,8 @@
-package server.graphql;
+//一下两种bean构建类
+// InitGraphqlGraphqls
+// InitGraphqlJava
+//为两种方式创建graphql对象选择一种即可
 
-public class info {
-}
+
+//GraphqlController
+//为自行实现接收参数方法，若使用此方法，可不使用graphql的starter相关依赖
