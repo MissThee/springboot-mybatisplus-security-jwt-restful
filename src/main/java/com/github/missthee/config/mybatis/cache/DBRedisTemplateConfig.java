af11b425803e0a0e9bd58ed08e4a40075250e940
Ma Teng <1163182715@qq.com>
2019-03-29 21:16:19 +0800

    update

--- src/main/java/com/github/missthee/config/mybatis/cache/DBRedisTemplateConfig.java
+++ src/main/java/com/github/missthee/config/mybatis/cache/DBRedisTemplateConfig.java
@@ -1,4 +1,4 @@
-package com.github.missthee.config.tkmapper.cache;
+package com.github.missthee.config.mybatis.cache;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
--- src/main/java/com/github/missthee/config/mybatis/cache/MybatisRedisCacheConfig.java
+++ src/main/java/com/github/missthee/config/mybatis/cache/MybatisRedisCacheConfig.java
@@ -1,7 +1,6 @@
-package com.github.missthee.config.tkmapper.cache;
+package com.github.missthee.config.mybatis.cache;
 
 import com.github.missthee.tool.ApplicationContextHolder;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.cache.Cache;
 import org.springframework.data.redis.core.RedisTemplate;
 
