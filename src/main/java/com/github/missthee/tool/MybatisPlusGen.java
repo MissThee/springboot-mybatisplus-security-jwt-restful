e5e5cf27d064694010ffc7ed5cc7e320d2223a16
Ma Teng <1163182715@qq.com>
2019-03-29 22:04:34 +0800

    update

--- src/main/java/com/github/missthee/tool/MybatisPlusGen.java
+++ src/main/java/com/github/missthee/tool/MybatisPlusGen.java
@@ -22,16 +22,99 @@ import org.apache.commons.lang3.StringUtils;
  */
 
 public class MybatisPlusGen {
+
+
+    public static void main(String[] args) {
+
+        String projectPath = System.getProperty("user.dir");
+
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig() {{
+            setOutputDir(projectPath + "/src/main/java");
+            setAuthor("mt");
+            setOpen(false);
+            setSwagger2(true);
+            setEnableCache(true);
+            setFileOverride(true);
+        }};
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig() {{
+            setUrl("jdbc:mysql://localhost:3306/mybatis_test_db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Asia/Shanghai&nullNamePatternMatchesAll=true&useSSL=false");
+            setDriverName("com.mysql.jdbc.Driver");
+            setUsername("user");
+            setPassword("1234");
+        }};
+        mpg.setDataSource(dsc);
+        final String functionName = "." + scanner("功能块名称。即生成文件的所属包名");
+
+        // 包配置
+        PackageConfig pc = new PackageConfig() {{
+            setParent("com.github.missthee.db");
+            setEntity("po.primary" + functionName);
+            setMapper("mapper.primary" + functionName);
+        }};
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+            }
+        };
+        {
+            String templatePath = "/templates/mapper.xml.ftl"; // 如果模板引擎是 freemarker
+            // 自定义输出配置
+            List<FileOutConfig> focList = new ArrayList<FileOutConfig>() {{
+                // 自定义配置会被优先输出
+                add(new FileOutConfig(templatePath) {
+                    @Override
+                    public String outputFile(TableInfo tableInfo) {
+                        // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                        return projectPath + "/src/main/resources/mybatis/mapper/primary/generate/"
+                                + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+                    }
+                });
+            }};
+            cfg.setFileOutConfigList(focList);
+        }
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig() {{
+            //为null时不生成相应文件
+            setXml(null);
+            setController(null);
+            setService(null);
+            setServiceImpl(null);
+        }};
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setEntityLombokModel(true);//lombok注解，无get、set
+        strategy.setSkipView(true);//跳过视图
+        strategy.setEntityColumnConstant(true);//跳过视图
+        strategy.setEntityTableFieldAnnotationEnable(true);//属性上加字段注解
+        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+        mpg.setStrategy(strategy);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
     /**
      * <p>
      * 读取控制台内容
      * </p>
      */
-    public static String scanner(String tip) {
+    private static String scanner(String tip) {
         Scanner scanner = new Scanner(System.in);
-        StringBuilder help = new StringBuilder();
-        help.append("请输入" + tip + "：");
-        System.out.println(help.toString());
+        System.out.println(("请输入" + tip + "："));
         if (scanner.hasNext()) {
             String ipt = scanner.next();
             if (StringUtils.isNotEmpty(ipt)) {
@@ -39,92 +122,5 @@ public class MybatisPlusGen {
             }
         }
         throw new MybatisPlusException("请输入正确的" + tip + "！");
-    }
-
-    public static void main(String[] args) {
-        // 代码生成器
-        AutoGenerator mpg = new AutoGenerator();
-
-        // 全局配置
-        GlobalConfig gc = new GlobalConfig();
-        gc.setOutputDir(System.getProperty("user.dir") + "/src/main/java");
-        gc.setAuthor("mt");
-        gc.setOpen(false);
-        // gc.setSwagger2(true); 实体属性 Swagger2 注解
-        mpg.setGlobalConfig(gc);
-
-        // 数据源配置
-        DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://localhost:3306/mybatis_test_db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Asia/Shanghai&nullNamePatternMatchesAll=true&useSSL=false");
-        // dsc.setSchemaName("public");
-        dsc.setDriverName("com.mysql.jdbc.Driver");
-        dsc.setUsername("user");
-        dsc.setPassword("1234");
-        mpg.setDataSource(dsc);
-
-        // 包配置
-        PackageConfig pc = new PackageConfig();
-        pc.setModuleName(scanner("模块名"));
-        pc.setParent("com.github.missthee.db");
-        pc.setController(null);
-        pc.setXml(null);
-        pc.setService(null);
-        pc.setServiceImpl(null);
-        pc.setEntity("po.primary");
-        pc.setMapper("mapper.primary");
-        mpg.setPackageInfo(pc);
-
-        // 自定义配置
-        InjectionConfig cfg = new InjectionConfig() {
-            @Override
-            public void initMap() {
-                // to do nothing
-            }
-        };
-
-        // 如果模板引擎是 freemarker
-        String templatePath = "/templates/mapper.xml.ftl";
-        // 如果模板引擎是 velocity
-        // String templatePath = "/templates/mapper.xml.vm";
-
-        // 自定义输出配置
-        List<FileOutConfig> focList = new ArrayList<>();
-        // 自定义配置会被优先输出
-        focList.add(new FileOutConfig(templatePath) {
-            @Override
-            public String outputFile(TableInfo tableInfo) {
-                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-                return System.getProperty("user.dir")  + "/src/main/resources/mybatis/mapper/primary/" + pc.getModuleName()
-                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
-            }
-        });
-
-        cfg.setFileOutConfigList(focList);
-        mpg.setCfg(cfg);
-
-        // 配置模板
-        TemplateConfig templateConfig = new TemplateConfig();
-
-        // 配置自定义输出模板
-        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
-        // templateConfig.setEntity("templates/entity2.java");
-        // templateConfig.setService();
-        // templateConfig.setController();
-
-        templateConfig.setXml(null);
-        mpg.setTemplate(templateConfig);
-
-        // 策略配置
-        StrategyConfig strategy = new StrategyConfig();
-        strategy.setNaming(NamingStrategy.underline_to_camel);
-        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
-        strategy.setCapitalMode(true);
-        strategy.setEntityLombokModel(true);
-        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
-        strategy.setControllerMappingHyphenStyle(true);
-        strategy.setTablePrefix(pc.getModuleName() + "_");
-        mpg.setStrategy(strategy);
-        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
-        mpg.execute();
     }
 }
