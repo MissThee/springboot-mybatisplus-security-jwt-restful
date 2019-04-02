400671680d9f5860c081cceb961432af4bf2ac7c
Ma Teng <1163182715@qq.com>
2019-04-02 08:11:24 +0800

    add bootstrap swaggerui

--- main-project/src/main/java/com/github/missthee/config/swagger/SwaggerConfig.java
+++ main-project/src/main/java/com/github/missthee/config/swagger/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package com.github.missthee.config.swagger;
 
+import org.assertj.core.util.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -17,6 +21,19 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.github.missthee.controller"))
                 .paths(PathSelectors.any())
+                .build()
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.<SecurityScheme>newArrayList(apiKey()));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("jwt的token值", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+//                .securityReferences()
+                .forPaths(PathSelectors.regex("/.*"))
                 .build();
     }
 }
