80c7214a1f957d6e57ad7579917779e463277847
马腾 <1163182715@qq.com>
2019-10-22 17:13:40 +0800

    update jackson config

--- main-project/common/src/main/java/com/github/common/config/json/LocalDateTimeSerializerConfig.java
+++ main-project/common/src/main/java/com/github/common/config/json/LocalDateTimeSerializerConfig.java
@@ -1,26 +1,64 @@
 package com.github.common.config.json;
 
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-
+//springboot默认使用jackson序列化，配置接收参数时使用的LocalXxx时间格式
 @Configuration
 public class LocalDateTimeSerializerConfig {
     @Value("${spring.jackson.date-format:yyyy-MM-dd HH:mm:ss}")
     private String pattern;
 
     @Bean
-    public LocalDateTimeSerializer localDateTimeDeserializer() {
+    public LocalDateTimeSerializer localDateTimeSerializer() {
         return new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Bean
+    public LocalDateTimeDeserializer localDateTimeDeserializer() {
+        return new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Bean
+    public LocalDateSerializer localDateSerializer() {
+        return new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+
+    @Bean
+    public LocalDateDeserializer localDateDeserializer() {
+        return new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    }
+    @Bean
+    public LocalTimeSerializer localTimeSerializer() {
+        return new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss"));
+    }
+
+    @Bean
+    public LocalTimeDeserializer localTimeDeserializer() {
+        return new LocalTimeDeserializer(DateTimeFormatter.ofPattern("HH:mm:ss"));
+    }
+    @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
-        return builder -> builder.serializerByType(LocalDateTime.class, localDateTimeDeserializer());
+        return builder ->
+                builder
+                        .serializerByType(LocalDateTime.class, localDateTimeSerializer())
+                        .deserializerByType(LocalDateTime.class, localDateTimeDeserializer())
+                        .serializerByType(LocalDate.class, localDateSerializer())
+                        .deserializerByType(LocalDate.class, localDateDeserializer())
+                        .serializerByType(LocalTime.class, localTimeSerializer())
+                        .deserializerByType(LocalTime.class, localTimeDeserializer())
+                ;
     }
 }
