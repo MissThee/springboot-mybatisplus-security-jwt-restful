a91fd27c4555b791377f67e797b443516089c322
马腾 <1163182715@qq.com>
2019-11-06 17:58:50 +0800

    更新字符串下划线驼峰转换工具类

--- main-project/common/src/main/java/com/github/common/tool/UnderLineAndHump.java
+++ main-project/common/src/main/java/com/github/common/tool/UnderLineAndHump.java
@@ -1,5 +1,7 @@
 package com.github.common.tool;
 
+import com.sun.javafx.binding.StringFormatter;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,7 +11,10 @@ public class UnderLineAndHump {
     /**
      * 下划线转驼峰
      */
-    public static String lineToHump(String str) {
+    private static String lineToHump(String str) {
+        if (str == null) {
+            return null;
+        }
         Matcher matcher = linePattern.matcher(str);
         StringBuffer sb = new StringBuffer();
         while (matcher.find()) {
@@ -19,19 +24,36 @@ public class UnderLineAndHump {
         return sb.toString();
     }
 
+    public static String lineToSmallHump(String str) {
+        String s = lineToHump(str);
+        return s.substring(0, 1).toLowerCase() + s.substring(1);
+    }
+
+    public static String lineToBigHump(String str) {
+        String s = lineToHump(str);
+        return s.substring(0, 1).toUpperCase() + s.substring(1);
+    }
+
     private static Pattern humpPattern = Pattern.compile("[A-Z]");
 
     /**
      * 驼峰转下划线,效率比上面高
      */
     public static String humpToLine(String str) {
+        if (str == null) {
+            return null;
+        }
         Matcher matcher = humpPattern.matcher(str);
         StringBuffer sb = new StringBuffer();
         while (matcher.find()) {
             matcher.appendReplacement(sb, "_" + matcher.group(0).toLowerCase());
         }
         matcher.appendTail(sb);
-        return sb.toString();
+        String s = sb.toString();
+        if (s.startsWith("_")) {
+            s = s.substring(1);
+        }
+        return s;
     }
 
 }
